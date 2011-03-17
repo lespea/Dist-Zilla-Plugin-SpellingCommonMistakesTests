8522b80cd59b4991bdb844b081e74b721e817c7f
Adam Lesperance <lespea@gmail.com>
2011-03-17 05:43:33 -0500

    Working version

---
+++ lib/Dist/Zilla/Plugin/SpellingCommonMistakesTests.pm
@@ -0,0 +1,52 @@
+use strict;
+use warnings;
+use utf8;
+
+package Dist::Zilla::Plugin::SpellingCommonMistakesTests;
+
+#ABSTRACT:  Release tests for common POD spelling mistakes
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+In your dist.ini file:
+
+    [PodSpellingTests]
+
+
+=head1 DESCRIPTION
+
+This is an extension of Dist::Zilla::Plugin::InlineFiles, providing the following file:
+
+    xt/release/common_spelling.t - a standard Pod::Spell::CommonMistakes test
+
+=cut
+
+
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+#  Happy ending
+1;
+
+
+## no critic (Documentation::RequirePodAtEnd)
+
+__DATA__
+___[ xt/release/common_spelling.t ]___
+#!/usr/bin/perl
+use strict; use warnings;
+
+use Test::More;
+
+eval "use Test::Pod::Spelling::CommonMistakes";
+if ( $@ ) {
+    plan skip_all => 'Test::Pod::Spelling::CommonMistakes required for testing POD';
+} else {
+    all_pod_files_ok();
+}
